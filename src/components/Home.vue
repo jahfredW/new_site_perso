1c626990a6caf7a714eeef15558978e62010f8f3
fredator <fred.gruwe@gmail.com>
2023-07-23 14:01:25 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,277 @@
+<template>
+    <div class="bg-deep-purple-accent-700">
+      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
+          <div class="flex flex-col mb-16 sm:text-center sm:mb-0">
+            <a href="/" class="mb-6 sm:mx-auto">
+              <div class="flex items-center justify-center w-12 h-12 rounded-full bg-teal-accent-400">
+                <svg class="w-10 h-10 text-deep-purple-900" stroke="currentColor" viewBox="0 0 52 52">
+                  <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                </svg>
+              </div>
+            </a>
+            <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+              <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
+                <span class="relative inline-block">
+                  <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-deep-purple-accent-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                    <defs>
+                      <pattern id="700c93bf-0068-4e32-aafe-ef5b6a647708" x="0" y="0" width=".135" height=".30">
+                        <circle cx="1" cy="1" r=".7"></circle>
+                      </pattern>
+                    </defs>
+                    <rect fill="url(#700c93bf-0068-4e32-aafe-ef5b6a647708)" width="52" height="24"></rect>
+                  </svg>
+                  <span class="relative">The</span>
+                </span>
+                quick, brown fox jumps over a lazy dog
+              </h2>
+              <p class="text-base text-indigo-100 md:text-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
+              </p>
+            </div>
+            <div>
+              <a
+                href="/"
+                class="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-deep-purple-accent-100 focus:shadow-outline focus:outline-none"
+              >
+                Get started
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            Brand new
+          </p>
+        </div>
+        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span class="relative inline-block">
+            <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+              <defs>
+                <pattern id="18302e52-9e2a-4c8e-9550-0cbb21b38e55" x="0" y="0" width=".135" height=".30">
+                  <circle cx="1" cy="1" r=".7"></circle>
+                </pattern>
+              </defs>
+              <rect fill="url(#18302e52-9e2a-4c8e-9550-0cbb21b38e55)" width="52" height="24"></rect>
+            </svg>
+            <span class="relative">The</span>
+          </span>
+          quick, brown fox jumps over a lazy dog
+        </h2>
+        <p class="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
+      </div>
+      <div class="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+              </svg>
+            </div>
+            <h6 class="mb-2 font-semibold leading-5">The deep ocean</h6>
+            <p class="mb-3 text-sm text-gray-900">
+              A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear.
+            </p>
+          </div>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+              </svg>
+            </div>
+            <h6 class="mb-2 font-semibold leading-5">When has justice</h6>
+            <p class="mb-3 text-sm text-gray-900">
+              Rough pomfret lemon shark plownose chimaera southern sandfish kokanee northern sea.
+            </p>
+          </div>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+              </svg>
+            </div>
+            <h6 class="mb-2 font-semibold leading-5">Organically grow</h6>
+            <p class="mb-3 text-sm text-gray-900">
+              A slice of heaven. O for awesome, this chocka full cuzzie is as rip-off as a cracker.
+            </p>
+          </div>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+              </svg>
+            </div>
+            <h6 class="mb-2 font-semibold leading-5">A slice of heaven</h6>
+            <p class="mb-3 text-sm text-gray-900">
+              Disrupt inspire and think tank, social entrepreneur but preliminary thinking think tank compelling.
+            </p>
+          </div>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+      </div>
+    </div>
+<!-- template projets -->
+
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div class="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
+      <h2 class="max-w-lg mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:mb-6 group">
+        <span class="inline-block mb-1 sm:mb-4">
+          There is no must in art<br class="hidden md:block" />
+          because art is free.
+        </span>
+        <div class="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100"></div>
+      </h2>
+      <p class="text-gray-700 lg:text-sm lg:max-w-md">
+        "Class is dead", says Foucault; however, according to Reicher, it is not so much class that is dead, but rather the absurdity, and some would say the paradigm, of class. However, the subject is interpolated into a
+        postpatriarchialist dialectic theory that includes consciousness as a totality.
+      </p>
+    </div>
+    <div class="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
+      <a href="/" aria-label="View Item">
+        <div class="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/3184311/pexels-photo-3184311.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500" alt="" />
+          <div class="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-4 text-lg font-bold text-gray-100">Mona Lisa</p>
+            <p class="text-sm tracking-wide text-gray-300">
+              Painted between 1503 and 1517, Da Vinci’s alluring portrait has been dogged by two questions since the day it was made: Who’s the subject and why is she smiling?
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="/" aria-label="View Item">
+        <div class="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="" />
+          <div class="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-4 text-lg font-bold text-gray-100">The Starry Night</p>
+            <p class="text-sm tracking-wide text-gray-300">
+              Vincent Van Gogh’s most popular painting, The Starry Night was created by Van Gogh at the asylum in Saint-Rémy, where he’d committed himself in 1889.
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="/" aria-label="View Item">
+        <div class="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="" />
+          <div class="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-4 text-lg font-bold text-gray-100">The Kiss</p>
+            <p class="text-sm tracking-wide text-gray-300">
+              Opulently gilded and extravagantly patterned, The Kiss, Gustav Klimt’s fin-de-siècle portrayal of intimacy, is a mix of Symbolism and Vienna Jugendstil, the Austrian variant of Art Nouveau.
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="/" aria-label="View Item">
+        <div class="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="" />
+          <div class="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-4 text-lg font-bold text-gray-100">The Harvesters</p>
+            <p class="text-sm tracking-wide text-gray-300">
+              Bruegel’s fanfare for the common man is considered one of the defining works of Western art. This composition was one of six created on the theme of the seasons.
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="text-center">
+      <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
+        View gallery
+        <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
+          <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
+        </svg>
+      </a>
+    </div>
+  </div>
+
+  <!-- technos -->
+
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div class="mx-auto mb-10 lg:max-w-xl sm:text-center">
+      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        Lorem Team
+      </p>
+      <p class="text-base text-gray-700 md:text-lg">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+      </p>
+    </div>
+    <div class="grid gap-10 mx-auto lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-4">
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Oliver Aguilerra</p>
+          <p class="text-sm text-gray-800">Product Manager</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Marta Clermont</p>
+          <p class="text-sm text-gray-800">Design Team Lead</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Anthony Geek</p>
+          <p class="text-sm text-gray-800">CTO, Lorem Inc.</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Alice Melbourne</p>
+          <p class="text-sm text-gray-800">Human Resources</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/3785077/pexels-photo-3785077.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Martin Garix</p>
+          <p class="text-sm text-gray-800">Bad boy</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Andrew Larkin</p>
+          <p class="text-sm text-gray-800">Backend Developer</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Sophie Denmo</p>
+          <p class="text-sm text-gray-800">Designer UI/UX</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="object-cover w-20 h-20 mb-2 rounded-full shadow" src="https://images.pexels.com/photos/3931553/pexels-photo-3931553.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+        <div class="flex flex-col items-center">
+          <p class="text-lg font-bold">Benedict Caro</p>
+          <p class="text-sm text-gray-800">Frontend Developer</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- connaissances -->
+
+
+
+
+  </template>
